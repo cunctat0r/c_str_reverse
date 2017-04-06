92df72ded25646027d7773fbfa576bfd1a141abc
Slava Petrov <slavik--petrov@yandex.ru>
2017-04-06 09:52:22 +0400

    Create main functioiality

--- c_str_reverse.c
+++ c_str_reverse.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+
+void inplace_reverse(char *);
 
 int main() {
-	printf("Hello All!");
+	char str[]="The long string to be reversed";
+	char * reversed;
+
+	printf("%s\n", str);
+	inplace_reverse(str);
+	printf("%s\n", str);
 	return 0;
+}
+
+void inplace_reverse(char * in_str) {
+	char * end = NULL;
+	if (in_str) {
+		end = in_str + strlen(in_str) - 1;
+
+		while (in_str < end) {
+			*in_str ^= *end;
+			*end ^= *in_str;
+			*in_str ^= *end;
+			in_str++;
+			end--;
+		}
+	}
 }
